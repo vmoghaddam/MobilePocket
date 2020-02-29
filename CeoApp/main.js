7dc404f0effe5cd05e77e902355c811c3a53a0e7
shahraeinisepehr@gmail.com <shahraeinisepehr@gmail.com>
2020-02-29 17:32:37 +0330

    cache

--- CeoApp/main.js
+++ CeoApp/main.js
@@ -1,11 +1,7 @@
-﻿if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js').then(function (registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
+﻿
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .catch(function (err) {
+            console.info('Service workers are not supported.');
         });
-    });
 }
